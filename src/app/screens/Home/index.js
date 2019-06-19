e2337ebc28861aca4c79c29c5be1fa86fb1a0341
sahkirteke <sahhusamettin@windowslive.com>
2019-06-20 02:24:43 +0300

    Add : Screens Home

---
+++ src/app/screens/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+      }
+      render(){
+          return(
+              <div>
+                BEN HUSAMaaaa YANIMDA BİLAL
+              </div>
+          )
+      }
+}
+export default Home 
